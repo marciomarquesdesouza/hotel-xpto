82e30ff884342d2a99e12c9976d36c15bc994b78
marciomarquesdesouza <marciomarques.contato@hotmail.com>
2022-07-19 23:16:33 -0300

    Inicialização de projeto

---
+++ src/main/java/br/com/hotelxpto/hotelxpto/HotelXptoApplication.java
@@ -0,0 +1,13 @@
+package br.com.hotelxpto.hotelxpto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelXptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelXptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hotelxpto/hotelxpto/HotelXptoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hotelxpto.hotelxpto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelXptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
